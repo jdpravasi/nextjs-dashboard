28cede237e8c2b07a275dd20e1383535afcf6eb6
jdpravasi <jdpravasi@gmail.com>
2024-04-24 17:04:50 +0530

    commmited

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+      <p className="flex items-center justify-center h-screen">
+        Invoices page
+      </p>
+    );
+  }
+  
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <p className="flex items-center justify-center h-screen">
+      This is the dashboard page
+    </p>
+  );
+}
